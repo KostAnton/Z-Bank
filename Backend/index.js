78867c5c39af1d8f386251ff6510ec620a49d19f
AntonUBUNTU <antonaleksandrkust@gmail.com>
2023-10-16 14:25:38 +0300

    Fixed .gitignore

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
+})
+//First GET Route
+app.post('/validate', (req, resp) => {
+  //Get The Data From The DB And Validate
+});
+
+app.get('/economics', (req, resp) => {
+  //Get the statistics data from the database and display  it for devs
+})
