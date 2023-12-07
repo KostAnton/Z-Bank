774f5c10d84a76b771fb5b236cbc6e46024aaf76
KostAnton <antonaleksandrkust@gmail.com>
2023-12-07 08:42:56 +0200

    New Changes

--- Z-BankAndroid/app/src/main/java/me/anton/z_bank/DAO/Transaction.java
+++ Z-BankAndroid/app/src/main/java/me/anton/z_bank/DAO/Transaction.java
@@ -8,34 +8,72 @@ import me.anton.z_bank.AccessLayer.APIAccessLayer;
 
 @IgnoreExtraProperties
 public class Transaction {
+
+    /** The user initiating the transaction (sender). */
     private User from;
+
+    /** The user receiving the transaction (receiver). */
     private User to;
+
+    /** The amount of the transaction. */
     private double amount;
+
+    /** Unique identifier for the transaction. */
     private String id;
 
-    public Transaction(){}
+    /**
+     * Default constructor for the Transaction class.
+     * Used for firebase purposes
+     */
+    public Transaction() {}
 
-    public Transaction(User from, User to, double amount){
+    /**
+     * Constructs a new Transaction with the specified parameters.
+     *
+     * @param from    The user initiating the transaction (sender).
+     * @param to      The user receiving the transaction (receiver).
+     * @param amount  The amount of the transaction.
+     */
+    public Transaction(User from, User to, double amount) {
         this.from = from;
         this.to = to;
         this.amount = amount;
         this.id = UUID.randomUUID().toString();
     }
 
-    public void transact(){
+    /**
+     * Initiates the transaction, making the necessary updates in the system.
+     * Calls an API layer to execute the transaction and updates the transaction lists for both sender and receiver.
+     */
+    public void transact() {
         APIAccessLayer.makeTransaction(this);
         this.from.getTransactions().add(this);
         this.to.getTransactions().add(this);
     }
 
+    /**
+     * Returns the user initiating the transaction (sender).
+     *
+     * @return The user initiating the transaction (sender).
+     */
     public User getFrom() {
         return from;
     }
 
+    /**
+     * Returns the user receiving the transaction (receiver).
+     *
+     * @return The user receiving the transaction (receiver).
+     */
     public User getTo() {
         return to;
     }
 
+    /**
+     * Returns the amount of the transaction.
+     *
+     * @return The amount of the transaction.
+     */
     public double getAmount() {
         return amount;
     }
--- Z-BankAndroid/app/src/main/java/me/anton/z_bank/DAO/User.java
+++ Z-BankAndroid/app/src/main/java/me/anton/z_bank/DAO/User.java
@@ -6,16 +6,43 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 @IgnoreExtraProperties
+/**
+ * The User class represents a user in a banking system.
+ * Each user has a unique identifier, a username, password, balance, savings balance,
+ * a list of friends' usernames, and a list of transactions.
+ */
 public class User {
+
+    /** Unique identifier for the user. */
     private String id;
+
+    /** User's username. */
     private String userName;
+
+    /** User's password. */
     private String password;
+
+    /** Current balance in the user's main account. */
     private double balance;
+
+    /** Current balance in the user's savings account. */
     private double savingsBalance;
+
+    /** List of usernames of friends associated with the user. */
     private ArrayList<String> friendsUserNames = new ArrayList<>();
+
+    /** List of transactions associated with the user. */
     private ArrayList<Transaction> transactions = new ArrayList<>();
 
-    public User(String userName, String password, double balance, double savingsBalance){
+    /**
+     * Constructs a new User with the specified parameters.
+     *
+     * @param userName        The username for the user.
+     * @param password        The password for the user.
+     * @param balance         The initial balance in the user's main account.
+     * @param savingsBalance  The initial balance in the user's savings account.
+     */
+    public User(String userName, String password, double balance, double savingsBalance) {
         this.userName = userName;
         this.password = password;
         this.balance = balance;
@@ -23,31 +50,71 @@ public class User {
         this.id = UUID.randomUUID().toString();
     }
 
-    public User(){}
+    /**
+     * Default constructor for the User class.
+     * Used for firebase purposes
+     */
+    public User() {}
+
+    /**
+     * Returns the unique identifier of the user.
+     *
+     * @return The unique identifier of the user.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns the current balance in the user's main account.
+     *
+     * @return The current balance in the user's main account.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Returns the current balance in the user's savings account.
+     *
+     * @return The current balance in the user's savings account.
+     */
     public double getSavingsBalance() {
         return savingsBalance;
     }
 
+    /**
+     * Returns the list of usernames of friends associated with the user.
+     *
+     * @return The list of usernames of friends associated with the user.
+     */
     public ArrayList<String> getFriendsUserNames() {
         return friendsUserNames;
     }
 
+    /**
+     * Returns the username of the user.
+     *
+     * @return The username of the user.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Returns the list of transactions associated with the user.
+     *
+     * @return The list of transactions associated with the user.
+     */
     public ArrayList<Transaction> getTransactions() {
         return transactions;
     }
 
+    /**
+     * Returns the password of the user.
+     *
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
